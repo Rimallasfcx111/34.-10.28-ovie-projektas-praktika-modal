d8ab61ea0caecd022006274ccecd7601f5ef8742
Rimallasfcx111 <113599111+Rimallasfcx111@users.noreply.github.com>
2022-11-01 18:49:50 +0200

    Pak

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 'use strict';
 console.log('app.js');
 
+// Nusitaikymas ==============================
+const els = {
+  addMovieBtn: document.getElementById('add-movie-btn'),
+  addMovieModal: document.getElementById('add-modal'),
+
+};
+console.log('els ===', els);
+
+// EVENT LISTENERS ==========================
+
+els.addMovieBtn.addEventListener('click', () => {
+
+   // parodyti modala
+    els.addMovieModal.classList.add('visible');
+
+});
